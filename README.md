998e49a8fe43823272819951df982e3ca7d49774
eric <skynocover@Gmail.com>
2020-02-24 21:42:31 +0800

    1.0.1

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SplitO
+
 SplitO for NCcode
+
+## Version
+
+### [1.0.1] 2020.02.24
+
+#### Fix
+
+- 修正了如果在windowns內被儲存換行符號會讀取不到的錯誤
+
+#### Change
+
+- 修改了產生檔案的寫法,避免傳值問題
--- spilt.go
+++ spilt.go
@@ -11,50 +11,48 @@ import (
 func main() {
 	filename := Getfile()
 	file := Readfile(filename)
-	program := Getprogram(file)
-	Creatfile(program)
+	creatFile(file)
 }
-func Creatfile(program [][]string) {
-	for i := 1; i < len(program); i++ { //i=檔案數 j=程式的行
-		file, err := os.Create(program[i][0]) //建立第一個陣列的檔案
-		if err != nil {
-			return
-		}
-		defer file.Close()
-		//逐漸將每一個陣列印到檔案裏面
-		for j := 0; j < len(program[i]); j++ {
-			file.WriteString(program[i][j] + "\n")
 
+func creatFile(program []string) {
+	var j = 0
+	for j < len(program) {
+		if strings.IndexAny(program[j], "O") == 0 { //找到開頭為O的行數
+			file, err := os.Create(program[j]) //依照當前文字建立檔案
+			if err != nil {
+				return
+			}
+			defer file.Close()
+			file.WriteString(program[j] + "\n") //寫入當前文字
+			j++
+			for strings.IndexAny(program[j], "O") != 0 { //不斷的寫入直到遇到下一次的O開頭
+				file.WriteString(program[j] + "\n")
+				j++
+				if j == len(program) { //如果已經到最後一行則return
+					return
+				}
+			}
+		} else { //若不是則繼續找下一行
+			j++
 		}
 	}
 }
 
-func Getprogram(file []string) [][]string {
-	var allprogram [][]string
-	i, j := 0, 0 //i表示紀錄上一次被輸入的行數 j表示當前行數
-	for j < len(file) {
-		if strings.IndexAny(file[j], "O") == 0 { 
-			allprogram = append(allprogram, file[i:j]) //將從上一次輸入到目前為止的程式碼丟進去
-			i = j //紀錄當前行數為上次輸入
-		}
-		j++
-	}
-	allprogram = append(allprogram, file[i:j]) //將最後一段也丟進去
-	return allprogram
-}
-
-func Getfile() string {
+func Getfile() (filename string) {
 	fmt.Println("Please enter filename: ")
-	var URL string
-	fmt.Scanln(&URL)
-	return URL
+	fmt.Scanln(&filename)
+	return
 }
 
-func Readfile(filename string) []string {
+func Readfile(filename string) (lines []string) {
 	content, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err)
 	}
-	lines := strings.Split(string(content), "\n")
-	return lines
+
+	lines = strings.Split(string(content), "\r\n")
+	if len(lines) == 1 { //如果不是以windowns的方式分行則只會有一段
+		lines = strings.Split(string(content), "\n")
+	}
+	return
 }
